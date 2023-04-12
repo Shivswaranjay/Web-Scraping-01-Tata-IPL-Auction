416e3dc4f5f4197587a684a9a094cc7c2a799602
Swaranjay <swaranjaybilure2002@gmail.com>
2023-04-13 00:28:36 +0530

    The IPL Auction 2023

---
+++ tataiplauction.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.iplt20.com/auction/2022"
+r = requests.get(url)
+# print(r)
+
+soup = BeautifulSoup(r.text, "lxml")
+# print(soup)
+
+table = soup.find("table", class_="ih-td-tab auction-tbl")
+# print(table)
+
+title = table.find_all("th")
+# print(title)
+
+header = []
+for i in title:
+    name = i.text
+    header.append(name)
+
+# print(header)
+
+df = pd.DataFrame(columns=header)
+print(df)
+
+rows = table.find_all("tr")
+for i in rows[1:]:
+    first_td = i.find_all("td")[0].find("div", class_="ih-pt-ic").text.strip()
+    data = i.find_all("td")[1:]
+    row = [tr.text for tr in data]
+    # print(data)
+
+    row.insert(0, first_td)
+    l = len(df)
+    df.loc[l] = row
+
+print(df)
+
+df.to_csv("ipl auction stats 2023.csv")
